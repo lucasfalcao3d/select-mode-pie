9db67eeb8dd77a01c3141951d64b57f817f43e36
lucasfalcao3d <57116182+lucasfalcao3d@users.noreply.github.com>
2019-11-06 21:08:43 -0300

    Update
    This code spawn the pie menu select mode for both Edit Mode and UV Editor.

--- select_mode_pie.py
+++ select_mode_pie.py
@@ -1,71 +1,97 @@
+bl_info = {
+    "name": "Select Mode Pie",
+    "author": "Lucas Falcao",
+    "version": (0, 4),
+    "description": "Pie Menu for Select Mode in Edit mode and UV Editor.",
+    "blender": (2, 80, 0),
+    "category": "3D view, UV"
+}
+
 import bpy
 from bpy.types import Menu
 
-# spawn an edit mode selection pie (run while object is in edit mode to get a valid output)
+# Spawn a pie menu for select mode in Edit Mode in the 3D View
 
 
-class IMAGE_MT_PIE_uvs_select_mode(Menu):
+class EDIT_SELECT_MODE(Menu):
+    # label is displayed at the center of the pie menu.
+    bl_label = "Edit Select Mode"
+    bl_idname = "pie.edit_select_mode"
+
+    def draw(self, context):
+        layout = self.layout
+
+        pie = layout.menu_pie()
+        # operator_enum will just spread all available options
+        # for the type enum of the operator on the pie
+        pie.operator_enum("mesh.select_mode", "type")
+        
+# Spawn a pie menu for select mode in UV Editor 
+
+class UV_SELECT_MODE(Menu):
     bl_label = "UV Select Mode"
-    bl_idname = "uv_select_mode.pie"
+    bl_idname = "pie.uv_select_mode"
 
     def draw(self, context):
         layout = self.layout
         
-        pie = layout.menu_pie()
-        # operator_enum will just spread all available options
-        # for the type enum of the operator on the pie
-        pie.operator_enum("tool_settings.mesh_select_mode", "type")
-        pie.operator_enum("tool_settings.uv_select_mode", "type")
         
-        layout.operator_context = 'INVOKE_REGION_WIN'
         tool_settings = context.tool_settings
+        pie = layout.menu_pie()
         
         # Do smart things depending on whether uv_select_sync is on.
 
         if tool_settings.use_uv_select_sync:
-            props = layout.operator("wm.context_set_value", text="Vertex", icon='VERTEXSEL')
-            props.value = "(True, False, False)"
-            props.data_path = "tool_settings.mesh_select_mode"
+            op = pie.operator("wm.context_set_value", text="Vertex", icon='VERTEXSEL')
+            op.value = "(True, False, False)"
+            op.data_path = "tool_settings.mesh_select_mode"
 
-            props = layout.operator("wm.context_set_value", text="Edge", icon='EDGESEL')
-            props.value = "(False, True, False)"
-            props.data_path = "tool_settings.mesh_select_mode"
+            op = pie.operator("wm.context_set_value", text="Edge", icon='EDGESEL')
+            op.value = "(False, True, False)"
+            op.data_path = "tool_settings.mesh_select_mode"
 
-            props = layout.operator("wm.context_set_value", text="Face", icon='FACESEL')
-            props.value = "(False, False, True)"
-            props.data_path = "tool_settings.mesh_select_mode"
-
+            op = pie.operator("wm.context_set_value", text="Face", icon='FACESEL')
+            op.value = "(False, False, True)"
+            op.data_path = "tool_settings.mesh_select_mode"            
+            
         else:
-            props = layout.operator("wm.context_set_string", text="Vertex", icon='UV_VERTEXSEL')
-            props.value = 'VERTEX'
-            props.data_path = "tool_settings.uv_select_mode"
+            op = pie.operator("wm.context_set_string", text="Vertex", icon='UV_VERTEXSEL')
+            op.value = 'VERTEX'
+            op.data_path = "tool_settings.uv_select_mode"
 
-            props = layout.operator("wm.context_set_string", text="Edge", icon='UV_EDGESEL')
-            props.value = 'EDGE'
-            props.data_path = "tool_settings.uv_select_mode"
+            op = pie.operator("wm.context_set_string", text="Edge", icon='UV_EDGESEL')
+            op.value = 'EDGE'
+            op.data_path = "tool_settings.uv_select_mode"
 
-            props = layout.operator("wm.context_set_string", text="Face", icon='UV_FACESEL')
-            props.value = 'FACE'
-            props.data_path = "tool_settings.uv_select_mode"
+            op = pie.operator("wm.context_set_string", text="Face", icon='UV_FACESEL')
+            op.value = 'FACE'
+            op.data_path = "tool_settings.uv_select_mode"
 
-            props = layout.operator("wm.context_set_string", text="Island", icon='UV_ISLANDSEL')
-            props.value = 'ISLAND'
-            props.data_path = "tool_settings.uv_select_mode"
+            op = pie.operator("wm.context_set_string", text="Island", icon='UV_ISLANDSEL')
+            op.value = 'ISLAND'
+            op.data_path = "tool_settings.uv_select_mode"
 
+
+classes = (
+EDIT_SELECT_MODE,
+UV_SELECT_MODE,
+)
 
 def register():
-    bpy.utils.register_class(IMAGE_MT_PIE_uvs_select_mode) 
-    
+    from bpy.utils import register_class
+    for cls in classes:
+        register_class(cls)
+        
+    wm = bpy.context.window_manager
+    km = wm.keyconfigs.addon.keymaps.new(name="Mesh")
+    kmi = km.keymap_items.new("wm.call_menu_pie", "SPACE", "PRESS").properties.name="pie.edit_select_mode"     
+        
     wm = bpy.context.window_manager
     km = wm.keyconfigs.addon.keymaps.new(name="UV Editor")
-    kmi = km.keymap_items.new("wm.call_menu_pie", "SPACE", "PRESS").properties.name="uv_select_mode.pie"
-
-
+    kmi = km.keymap_items.new("wm.call_menu_pie", "SPACE", "PRESS").properties.name="pie.uv_select_mode"  
+        
+    
 def unregister():
-    bpy.utils.unregister_class(IMAGE_MT_PIE_uvs_select_mode)
-
-
-if __name__ == "__main__":
-    register()
-
-
+    from bpy.utils import unregister_class
+    for cls in reversed(classes):
+        unregister_class(cls)
